f150c78c98aec0ac7813712c16dda5f1ee406795
Jeffrey Carpenter <i8degrees@gmail.com>
2016-04-05 18:21:49 -0500

    Bootstrapping...

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
 const exec = require('child_process').exec;
+var app = express();
+
+router.get('/', function(req, res, next) {
+  res.json({response: 'OK'});
+});
 
 router.get('/reboot', function(req, res, next) {
 
-  if(app.get('DEBUG') === true) {
-    console.log('DEBUG');
-    return;
+  if(res.app.get('DEBUG') === false) {
+    const child = exec('shutdown -r now',
+      (error, stdout, stderr) => {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+          console.log(`exec error: ${error}`);
+        }
+    });
   }
 
-  const child = exec('shutdown -r now',
-    (error, stdout, stderr) => {
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-      if (error !== null) {
-        console.log(`exec error: ${error}`);
-      }
-  });
-
-  res.json('OK');
+  res.json({response: 'OK'});
 });
 
 module.exports = router;
